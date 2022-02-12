4635780891424548f5cfc20db904eac5c59b0626
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-12 20:57:02 +0000

    score prop passed down

--- src/containers/GameContainer/index.js
+++ src/containers/GameContainer/index.js
@@ -1,11 +1,14 @@
+import { useEffect, useState } from "react";
+
 import { CardGrid } from "../../components/CardGrid";
 
 import { Score } from "../../components/Score";
 
 export const GameContainer = () => {
+  const [numberOfFailedMatches, setNumberOfFailedMatches] = useState(0);
   return (
     <>
-      <Score />
+      <Score numberOfFailedMatches={numberOfFailedMatches} />
       <CardGrid />
     </>
   );
